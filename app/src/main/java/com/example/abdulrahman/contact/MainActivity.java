a08b22ac77b67e50b89c0f3d3c1573745b9b21cd
unknown <abdxax@gmail.com>
2017-11-05 02:36:33 +0300

    Contact provider

---
+++ app/src/main/java/com/example/abdulrahman/contact/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.abdulrahman.contact;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Build;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView=(ListView) findViewById(R.id.cont);
+       // ReadCount();
+        //ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
+        //listView.setAdapter(arrayAdapter);
+        CheckPermission();
+
+    }
+final int SQO=90;
+    public void CheckPermission(){
+        if (Build.VERSION.SDK_INT>=23){
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)!= PackageManager.PERMISSION_GRANTED){
+
+                requestPermissions(new String[]{
+                        Manifest.permission.READ_CONTACTS
+                },SQO);
+            }
+        }
+        ReadCount();
+    }
+
+    ArrayList<contactm> arrayList=new ArrayList<>();
+    public void ReadCount(){
+        Cursor cursor=getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null);
+    if (cursor.moveToFirst()){
+        do{
+            String name=cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String number=cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            arrayList.add(new contactm(name,number));
+        }  while (cursor.moveToNext());
+    }
+    else {
+        Toast.makeText(this, "is empty",Toast.LENGTH_LONG).show();
+    }
+        ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+
+    }
+public void Lis(){
+    ReadCount();
+
+}
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case SQO:
+                if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                   ReadCount();
+                }
+                else {
+                    Toast.makeText(this, "not connect permission ",Toast.LENGTH_LONG).show();
+                }
+                break;
+                default:
+                    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/abdulrahman/contact/contactm.java
@@ -0,0 +1,15 @@
+package com.example.abdulrahman.contact;
+
+/**
+ * Created by Abdulrahman on 05/11/17.
+ */
+
+public class contactm {
+    public String name;
+    public String number;
+
+    public contactm(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+}
